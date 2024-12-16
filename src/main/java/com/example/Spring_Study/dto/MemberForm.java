63c0a48220c20c235359992f908f313b9c6f9916
Baskervir <persona903@gmail.com>
2024-12-17 04:21:54 +0900

    create member page

---
+++ src/main/java/com/example/Spring_Study/dto/MemberForm.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Study.dto;
+
+public class MemberForm {
+    private String email;
+    private String password;
+
+    public MemberForm(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+
+}
---
+++ src/main/java/com/example/Spring_Study/entity/Member.java
@@ -0,0 +1,4 @@
+package com.example.Spring_Study.entity;
+
+public class Member {
+}
