b29dc9fb0b721245eb51eba59623ea0b710ada4c
Baskervir <persona903@gmail.com>
2024-12-17 04:24:35 +0900

    ing

--- src/main/java/com/example/Spring_Study/controller/MemberController.java
+++ src/main/java/com/example/Spring_Study/controller/MemberController.java
@@ -1,8 +1,15 @@
 package com.example.Spring_Study.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
-    
+    @GetMapping("members/new")
+    public String newMemberForm() {
+        return "members/new";
+    }
+
+    @PostMapping("/members")
 }
