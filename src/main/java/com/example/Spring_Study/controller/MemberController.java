f786da0e8d8b7177524c18559faa278f035bccfb
Baskervir <persona903@gmail.com>
2024-12-17 20:41:20 +0900

    refactoring MemberController

--- src/main/java/com/example/Spring_Study/controller/MemberController.java
+++ src/main/java/com/example/Spring_Study/controller/MemberController.java
@@ -3,13 +3,18 @@ package com.example.Spring_Study.controller;
 import com.example.Spring_Study.dto.MemberForm;
 import com.example.Spring_Study.entity.Member;
 import com.example.Spring_Study.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class MemberController {
+    private static final Logger log = LoggerFactory.getLogger(MemberController.class);
     @Autowired
     MemberRepository memberRepository;
 
@@ -20,11 +25,16 @@ public class MemberController {
 
     @PostMapping("/join")
     public String join(MemberForm memberForm) {
-        System.out.println(memberForm.toString());
+        log.info(memberForm.toString());
+//        System.out.println(memberForm.toString());
+
         Member member = memberForm.toEntity();
-        System.out.println(member.toString());
+        log.info(member.toString());
+//        System.out.println(member.toString());
+
         Member saved = memberRepository.save(member);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+//        System.out.println(saved.toString());
         return "";
     }
 }
