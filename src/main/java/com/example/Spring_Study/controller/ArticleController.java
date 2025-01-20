b00d17aafcb87f84e1a097b0c8cd69613b393c06
Baskervir <persona903@gmail.com>
2025-01-20 20:11:44 +0900

    added @Slf4j

--- src/main/java/com/example/Spring_Study/controller/ArticleController.java
+++ src/main/java/com/example/Spring_Study/controller/ArticleController.java
@@ -3,16 +3,16 @@ package com.example.Spring_Study.controller;
 import com.example.Spring_Study.dto.ArticleForm;
 import com.example.Spring_Study.entity.Article;
 import com.example.Spring_Study.repository.ArticleRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class ArticleController {
-    private static final Logger log = LoggerFactory.getLogger(ArticleController.class);
+
     @Autowired
     private ArticleRepository articleRepository;
 
--- src/main/java/com/example/Spring_Study/controller/MemberController.java
+++ src/main/java/com/example/Spring_Study/controller/MemberController.java
@@ -4,8 +4,6 @@ import com.example.Spring_Study.dto.MemberForm;
 import com.example.Spring_Study.entity.Member;
 import com.example.Spring_Study.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Slf4j
 @Controller
 public class MemberController {
-    private static final Logger log = LoggerFactory.getLogger(MemberController.class);
     @Autowired
     MemberRepository memberRepository;
 
